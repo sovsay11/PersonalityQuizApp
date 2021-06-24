f217742f0ae89269f7c15faecb1fdd85bb67ee9f
Noah <sovannarasay11@gmail.com>
2021-06-24 00:15:39 -0600

    Added an event to record the value of a selected radio button in the first question bank.

--- PersonalityQuizApp/PersonalityQuizApp/MainPage.xaml.cs
+++ PersonalityQuizApp/PersonalityQuizApp/MainPage.xaml.cs
@@ -10,9 +10,31 @@ namespace PersonalityQuizApp
 {
     public partial class MainPage : ContentPage
     {
+        string value;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void BtnShowResults_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Question1_CheckChanged(object sender, CheckedChangedEventArgs e)
+        {
+            // This grabs the sender object (sent by the radio button)
+            // and casts it as a RadioButton
+            RadioButton selectedButton = (RadioButton)sender;
+
+            // We can then use the cast object (which is now a radio button)
+            // to check the value of the object
+            if (selectedButton.IsChecked)
+            {
+                // Assuming the radio button is selected, we can display its value
+                value = selectedButton.Value.ToString();
+                DisplayAlert(value, value, "Close");
+            }
         }
     }
 }
